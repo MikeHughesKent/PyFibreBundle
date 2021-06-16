ebadf06875b03ec704153ce4fac02a5a0b67ef56
unknown <AOG@NBSPS46.ad.kent.ac.uk>
2021-06-16 15:42:12 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 14:30:38 2021
+
+@author: AOG
+"""
+
---
+++ test/context.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 14:37:55 2021
+
+@author: AOG
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
