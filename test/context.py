0a0f12e8e04f0eb2ff1f736c017f91085cea9bb7
unknown <m.r.hughes@kent.ac.uk>
2021-07-03 21:27:15 +0100

    Added context.py

---
+++ test/context.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  3 21:23:46 2021
+
+@author: AOG
+"""
+
+import sys, os
+testdir = os.path.dirname(__file__)
+srcdir = '../'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
