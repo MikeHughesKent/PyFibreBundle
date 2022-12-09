406523a3346b47556f23a2d9f7d80ed66552710d
unknown <m.r.hughes@kent.ac.uk>
2022-12-09 07:57:55 +0000

    Added test all

---
+++ test/test_all.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Runs all tests.
+
+@author: Mike Hughes, Applied Optics Group, University of Kent
+"""
+
+print("\ntest_simple_processing")
+print("-------------------------")
+import test_simple_processing
+
+print("\ntest_masking")
+print("-------------------------")
+import test_masking
+
+print(" \ntest_find_cores")
+print("-------------------------")
+import test_find_cores
+
+print("\ntest_mosaic")
+print("-------------------------")
+import test_mosaic
+
+print("\ntest_pybundle_oop")
+print("-------------------------")
+import test_pybundle_oop
+
+print("\ntest_recon_tri_interp")
+print("-------------------------")
+import test_recon_tri_interp
+
+print("\ntest_super_res")
+print("-------------------------")
+import test_super_res
